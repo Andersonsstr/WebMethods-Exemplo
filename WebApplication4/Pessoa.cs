dd1b500d5474e62609af5c0c771a25bcff665430
Anderson Silva <andersonsstr@gmail.com>
2021-05-20 08:46:09 -0300

    Add project files.

---
+++ WebApplication4/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public string idade { get; set; }
+        public string cpf { get; set; }
+        public string idBotao { get; set; }
+
+        public Pessoa(string nome, string idade, string cpf, string idBotao)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.cpf = cpf;
+            this.idBotao = idBotao;
+        }
+    }
+}
